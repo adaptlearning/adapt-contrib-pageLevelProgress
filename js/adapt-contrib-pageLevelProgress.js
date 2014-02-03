bc13abcb30f9216ac4b9e164a33539f1f0f60532
Daryl Hedley <darylhedley@hotmail.com>
2014-02-03 10:45:03 +0000

    Updated progress to allow scrolling and show completion

--- js/adapt-contrib-pageLevelProgress.js
+++ js/adapt-contrib-pageLevelProgress.js
@@ -3,8 +3,45 @@ define(function(require) {
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
 
-	Adapt.on('router:page', function() {
-		console.log('page level should start now');
+	var PageLevelProgressView = Backbone.View.extend({
+
+		className: "page-level-progress",
+
+		initialize: function() {
+			this.listenTo(Adapt, 'remove', this.remove);
+			console.log('this:', this);
+			this.render();
+		},
+
+		events: {
+			'click .page-level-progress-item a': 'scrollToPageElement'
+		},
+
+		scrollToPageElement: function(event) {
+			event.preventDefault();
+			var currentComponentSelector = '.' + $(event.currentTarget).attr('data-page-level-progress-id');
+			var $currentComponent = $(currentComponentSelector);
+			$(window).scrollTo($currentComponent, {offset:{top:-$('.navigation').height()}});
+			Adapt.trigger('page:scrollTo', currentComponentSelector);
+		},
+
+		render: function() {
+			var data = this.collection.toJSON();
+	        var template = Handlebars.templates["pageLevelProgress"];
+	        this.$el.html(template({components:data})).appendTo('body');
+	        return this;
+		}
+
 	});
 
-});
+	function setupPageLevelProgress(currentPageComponents) {
+		var componentsCollection = new Backbone.Collection(currentPageComponents);
+		new PageLevelProgressView({collection:componentsCollection})
+	}
+
+	Adapt.on('router:page', function(pageModel) {
+		var currentPageComponents = pageModel.findDescendants('components').where({'_isAvailable': true});
+		setupPageLevelProgress(currentPageComponents);
+	});
+
+})
