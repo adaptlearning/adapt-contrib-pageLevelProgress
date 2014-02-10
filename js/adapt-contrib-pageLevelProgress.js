2c021648db7ae3035b5068f222eb22c12e806ef1
Daryl Hedley <darylhedley@hotmail.com>
2014-02-10 15:24:16 +0000

    Updated pageLevelProgress to work with v1.1

--- js/adapt-contrib-pageLevelProgress.js
+++ js/adapt-contrib-pageLevelProgress.js
@@ -1,7 +1,14 @@
+/*
+* Page Level Progress
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Daryl Hedley <darylhedley@hotmail.com>
+*/
+
 define(function(require) {
 
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
+	var Drawer = require('coreJS/drawer');
 
 	var PageLevelProgressView = Backbone.View.extend({
 
@@ -9,7 +16,6 @@ define(function(require) {
 
 		initialize: function() {
 			this.listenTo(Adapt, 'remove', this.remove);
-			console.log('this:', this);
 			this.render();
 		},
 
@@ -23,25 +29,85 @@ define(function(require) {
 			var $currentComponent = $(currentComponentSelector);
 			$(window).scrollTo($currentComponent, {offset:{top:-$('.navigation').height()}});
 			Adapt.trigger('page:scrollTo', currentComponentSelector);
+			Adapt.trigger('drawer:closeDrawer');
 		},
 
 		render: function() {
 			var data = this.collection.toJSON();
 	        var template = Handlebars.templates["pageLevelProgress"];
-	        this.$el.html(template({components:data})).appendTo('body');
+	        this.$el.html(template({components:data}));
 	        return this;
 		}
 
 	});
 
-	function setupPageLevelProgress(currentPageComponents) {
-		var componentsCollection = new Backbone.Collection(currentPageComponents);
-		new PageLevelProgressView({collection:componentsCollection})
+	var PageLevelProgressNavigationView = Backbone.View.extend({
+
+		tagName: 'a',
+
+		className: 'page-level-progress-navigation',
+
+		initialize: function() {
+			this.listenTo(Adapt, 'remove', this.remove);
+			this.listenTo(this.collection, 'change:_isComplete', this.updateProgressBar);
+			this.$el.attr('href', '#');
+			this.render();
+			this.updateProgressBar();
+		},
+
+		events: {
+			'click': 'onProgressClicked'
+		},
+
+		render: function() {
+			var data = this.collection.toJSON();
+	        var template = Handlebars.templates["pageLevelProgressNavigation"];
+	        $('.navigation-drawer-toggle-button').after(this.$el.html(template({components:data})));
+	        return this;
+		},
+
+		updateProgressBar: function() {
+			var componentCompletionRatio = this.collection.where({_isComplete:true}).length / this.collection.length;
+			var percentageOfCompleteComponents = componentCompletionRatio*100;
+
+			this.$('.page-level-progress-navigation-bar').css('width', percentageOfCompleteComponents+'%');
+
+		},
+
+		onProgressClicked: function(event) {
+			event.preventDefault();
+			Adapt.drawer.triggerCustomView(new PageLevelProgressView({collection:this.collection}).$el);
+		}
+
+	});
+
+	function setupPageLevelProgress(enabledProgressComponents) {
+
+		var componentsCollection = new Backbone.Collection(enabledProgressComponents);
+
+		new PageLevelProgressNavigationView({collection:componentsCollection});
+	
 	}
+
+	/*Adapt.on('app:dataReady', function() {
+		var drawerObject = {
+			title: Adapt.config.get('_pageLevelProgress').title,
+			description: Adapt.config.get('_pageLevelProgress').description,
+			className: 'drawer-page-level-progress'
+		};
+
+		Adapt.drawer.addItem(drawerObject, 'pageLevelProgress:show');
+	});*/
 
 	Adapt.on('router:page', function(pageModel) {
 		var currentPageComponents = pageModel.findDescendants('components').where({'_isAvailable': true});
-		setupPageLevelProgress(currentPageComponents);
+
+		var enabledProgressComponents = _.filter(currentPageComponents, function(component) {
+			return component.attributes._pageLevelProgress._isEnabled;
+		});
+
+		setupPageLevelProgress(enabledProgressComponents);
+
 	});
 
 })
