a990660ef4a4aae34f4cf0207cca7cd0ffffe4e2
James Rea <James.Rea@kineo.com>
2018-05-22 15:27:17 +0100

    new variable added to exclude assessments from counting towards PLP- _excludeAssessments in contentObjects.json

--- js/completionCalculations.js
+++ js/completionCalculations.js
@@ -33,8 +33,10 @@ define([
             assessmentComponentsTotal = assessmentComponents.length;
             assessmentComponentsCompleted = getComponentsInteractionCompleted(assessmentComponents).length;
 
-            subProgressCompleted = contentObjectModel.get("_subProgressComplete") || 0;
-            subProgressTotal = contentObjectModel.get("_subProgressTotal") || 0;
+            if (contentObjectModel.get('_pageLevelProgress')._includeAssessments) {
+                subProgressCompleted = contentObjectModel.get("_subProgressComplete") || 0;
+                subProgressTotal = contentObjectModel.get("_subProgressTotal") || 0;
+            }
 
             var pageCompletion = {
                 "subProgressCompleted": subProgressCompleted,
