96d10789c939463d501102fe5a725602f6c6f7d2
Oliver Foster <oliver.foster@kineo.com>
2019-11-14 14:33:03 +0000

    issue/2581 added js non-destructive render

--- js/PageLevelProgressIndicatorView.js
+++ js/PageLevelProgressIndicatorView.js
@@ -10,7 +10,6 @@ define([
       this.ariaLabel = options.ariaLabel || '';
       this.type = options.type || this.model.get('_type');
       this.addClasses();
-      this.checkAria();
       this.setUpEventListeners();
       this.render();
       this.refresh();
@@ -24,8 +23,15 @@ define([
     },
 
     checkAria: function() {
-      if (this.ariaLabel) return;
-      this.$el.attr('aria-hidden', true);
+      if (!this.ariaLabel) {
+        this.$el.attr('aria-hidden', true);
+        return;
+      }
+      var data = this.model.toJSON();
+      data.ariaLabel = this.ariaLabel;
+      data.type = this.type;
+      this.model.set('percentageComplete', percentage);
+      this.$('.js-indicator-aria-label').html(Handlebars.compile(this.ariaLabel)(data));
     },
 
     setUpEventListeners: function() {
@@ -37,7 +43,10 @@ define([
 
     refresh: function() {
       this.checkCompletion();
-      this.render();
+      this.checkAria();
+      this.$('.js-indicator-bar').css({
+        width: this.calculatePercentage() + '%'
+      });
     },
 
     checkCompletion: function() {
