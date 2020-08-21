4d7c32c8cd47233c2c5173ce9b94b598b0891c09
Oliver Foster <oliver.foster@kineo.com>
2020-08-21 20:45:54 +0100

    issue/2762 Updated to use renderTo and disable _isLocked

--- js/PageLevelProgressView.js
+++ js/PageLevelProgressView.js
@@ -17,13 +17,24 @@ define([
       this.addChildren();
     },
 
-    scrollToPageElement: function(event) {
+    scrollToPageElement: async function(event) {
       if (event && event.preventDefault) event.preventDefault();
 
       var $target = $(event.currentTarget);
       if ($target.is('.is-disabled')) return;
 
-      var currentComponentSelector = '.' + $target.attr('data-pagelevelprogress-id');
+      var id = $target.attr('data-pagelevelprogress-id');
+      var model = Adapt.findById(id);
+
+      if (!model.get('_isRendered')) {
+        try {
+          await Adapt.parentView.renderTo(id);
+        } catch(err) {
+          return;
+        }
+      }
+
+      var currentComponentSelector = '.' + id;
 
       Adapt.once('drawer:closed', function() {
         Adapt.scrollTo(currentComponentSelector, { duration: 400 });
