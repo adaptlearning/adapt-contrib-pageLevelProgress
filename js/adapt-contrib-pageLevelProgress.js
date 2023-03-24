e8321b0668af2b34d12e23430b2d99e2520e5816
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2023-03-24 15:24:05 +0000

    Fix: updated from collection === null to !collection

--- js/adapt-contrib-pageLevelProgress.js
+++ js/adapt-contrib-pageLevelProgress.js
@@ -136,7 +136,7 @@ class PageLevelProgress extends Backbone.Controller {
     if (viewType === 'course' && coursePLPConfig._showAtCourseLevel !== true) return;
 
     const collection = getPageLevelProgressItems(pageModel);
-    if (collection === null) return;
+    if (!collection) return;
 
     $('.nav__drawer-btn').after(new PageLevelProgressNavigationView({
       model: pageModel,
