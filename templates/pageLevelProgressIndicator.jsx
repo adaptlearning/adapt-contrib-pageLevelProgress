2bfb99a5ebc6bff9db79eec7c84203c1ea61253d
Joseph Replin <joe.replin@kineo.com>
2025-10-28 17:13:50 -0500

    Revert "Update pageLevelProgressIndicator.jsx"
    This reverts commit 4c9f7686b8997b82bd8e5d8756fbd97b0f72deee.

--- templates/pageLevelProgressIndicator.jsx
+++ templates/pageLevelProgressIndicator.jsx
@@ -3,38 +3,8 @@ import { compile } from 'core/js/reactHelpers';
 
 export default function PageLevelProgressIndicator (props) {
   const {
-    ariaLabel,
-    _isOptional
+    ariaLabel
   } = props;
-
-  // Build aria-label with optional prefix if needed
-  const compiledAriaLabel = ariaLabel ? compile(ariaLabel, props) : '';
-  const fullAriaLabel = _isOptional ? `Optional. ${compiledAriaLabel}` : compiledAriaLabel;
-
-  // Only render wrapper group when optional label exists
-  if (_isOptional) {
-    return (
-      <span className='pagelevelprogress__indicator-group'>
-        <span className='pagelevelprogress__indicator'>
-          <span className="pagelevelprogress__indicator-inner">
-
-            <span className="pagelevelprogress__indicator-bar"></span>
-
-            {ariaLabel &&
-            <span className="aria-label">
-              {fullAriaLabel}
-            </span>
-            }
-
-          </span>
-        </span>
-
-        <span className="pagelevelprogress__indicator-label" aria-hidden="true">
-          Optional
-        </span>
-      </span>
-    );
-  }
 
   return (
     <span className='pagelevelprogress__indicator'>
@@ -44,7 +14,7 @@ export default function PageLevelProgressIndicator (props) {
 
         {ariaLabel &&
         <span className="aria-label">
-          {fullAriaLabel}
+          {compile(ariaLabel, props)}
         </span>
         }
 
